ff16e3283cb899b96b59e3035133e8292890c35b
Bojan <dbojan@gmail.com>
2024-06-17 14:13:01 -0300

    hello from 1.1.2

--- gorel.go
+++ gorel.go
@@ -5,6 +5,6 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome from release 1.1.1!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome from release 1.1.2!", name)
 	return message
 }
